c28c0112672ce316dedbb53ef1ab38dbdb657649
Armen <armen.arsh@gmail.com>
2023-06-13 14:33:21 +0400

    css

--- myjs.js
+++ myjs.js
@@ -1,22 +1,31 @@
-// Add an event listener to the document to handle dynamically added <p> elements
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function(event) {
-      // Check if the clicked element is a <p> element
-      if (event.target.tagName === 'P') {
-        const paragraph = event.target;
-        const text = paragraph.textContent;
+// Create a mutation observer instance
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      // Check if new nodes have been added
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach(function(node) {
+          // Check if the node is a <p> element
+          if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'P') {
+            const paragraph = node;
+            const text = paragraph.textContent;
   
-        // Check if the text starts with more than three dashes
-        if (text.startsWith('---')) {
-          // Create a new <h2> element
-          const heading = document.createElement('h2');
+            // Check if the text starts with more than three dashes
+            if (text.startsWith('---')) {
+              // Create a new <h2> element
+              const heading = document.createElement('h2');
   
-          // Copy the content from the <p> element to the <h2> element
-          heading.textContent = text;
+              // Copy the content from the <p> element to the <h2> element
+              heading.textContent = text;
   
-          // Replace the <p> element with the new <h2> element
-          paragraph.parentNode.replaceChild(heading, paragraph);
-        }
+              // Replace the <p> element with the new <h2> element
+              paragraph.parentNode.replaceChild(heading, paragraph);
+            }
+          }
+        });
       }
     });
   });
+  
+  // Observe changes in the notes section
+  const notesSection = document.getElementById('notesSection');
+  observer.observe(notesSection, { childList: true, subtree: true });
